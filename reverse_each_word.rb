47f8ea0dd60571a8226c178ff580d55f56d5c7ce
taroj khan <tarujkhan@yahoo.com>
2020-03-05 03:23:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,10 @@
+require 'pry'
 # def reverse_each_word(sentence)
 #   sentence.split.collect {|word| word.reverse}.join(" ")
 # end
 
 def reverse_each_word(sentence)
+  binding.pry 
  reversed_sentence = sentence.split.each do {|word| word.reverse }
   puts reversed_sentence
 end
