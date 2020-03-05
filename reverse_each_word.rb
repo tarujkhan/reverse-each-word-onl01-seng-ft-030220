ad0f3fc45a6172cbab989319b0f71dc947d60436
taroj khan <tarujkhan@yahoo.com>
2020-03-05 03:08:01 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+# def reverse_each_word(sentence)
+#   sentence.split.collect {|word| word.reverse}.join(" ")
+# end
+
+def reverse_each_word(sentence)
+ reversed_sentence = sentence.each do {|word| word.reverse }
+  puts reversed_sentence
+end
+end
+  
