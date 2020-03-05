622af95311c36e95bc6e37b9641a1e90bf5ae4ed
taroj khan <tarujkhan@yahoo.com>
2020-03-05 03:32:27 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+require 'pry'
+# def reverse_each_word(sentence)
+#   sentence.split.collect {|word| word.reverse}.join(" ")
+# end
+
+def reverse_each_word(sentence)
+  
+
+ sentence.split.collect {|word| word.reverse}.join(" ") 
+ 
+end
+
+  
